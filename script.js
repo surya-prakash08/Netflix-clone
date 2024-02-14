4edb681ec4c6fd5ddf81c36c22287ffc0df7fc8e
surya-prakash08 <114825044+surya-prakash08@users.noreply.github.com>
2024-02-14 21:16:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+var dropdownContent = document.querySelector('.dropdown-content')
+    var mainContent = document.querySelector('.faqbox')
+    mainContent.addEventListener('click', function () {
+        if (dropdownContent.style.display === '' || dropdownContent.style.display === 'none') {
+
+            dropdownContent.style.display = 'block'
+
+
+        }
+        else {
+            // dropdownContent.classList.remove('toggle')
+            // dropdownContent.classList.add('dropdown-content');
+
+            dropdownContent.style.display = 'none'
+        }
+    })
+
+
+
+    var dropdownContent2 = document.querySelector('.dc2')
+    var mainContent = document.querySelector('.fb2')
+    mainContent.addEventListener('click', function () {
+        if (dropdownContent2.style.display === '' || dropdownContent.style.display === 'none') {
+
+            dropdownContent2.style.display = 'block'
+
+
+        }
+        else {
+            // dropdownContent.classList.remove('toggle')
+            // dropdownContent.classList.add('dropdown-content');
+
+            dropdownContent2.style.display = 'none'
+        }
+    })
+    var dropdownContent3 = document.querySelector('.dc3')
+    var mainContent = document.querySelector('.fb3')
+    mainContent.addEventListener('click', function () {
+        if (dropdownContent3.style.display === '' || dropdownContent.style.display === 'none') {
+
+            dropdownContent3.style.display = 'block'
+
+
+        }
+        else {
+            // dropdownContent.classList.remove('toggle')
+            // dropdownContent.classList.add('dropdown-content');
+
+            dropdownContent3.style.display = 'none'
+        }
+    })
+    var dropdownContent4 = document.querySelector('.dc4')
+    var mainContent = document.querySelector('.fb4')
+    mainContent.addEventListener('click', function () {
+        if (dropdownContent4.style.display === '' || dropdownContent.style.display === 'none') {
+
+            dropdownContent4.style.display = 'block'
+
+
+        }
+        else {
+            // dropdownContent.classList.remove('toggle')
+            // dropdownContent.classList.add('dropdown-content');
+
+            dropdownContent4.style.display = 'none'
+        }
+    })
+    var dropdownContent5 = document.querySelector('.dc5')
+    var mainContent = document.querySelector('.fb5')
+    mainContent.addEventListener('click', function () {
+        if (dropdownContent5.style.display === '' || dropdownContent.style.display === 'none') {
+
+            dropdownContent5.style.display = 'block'
+
+
+        }
+        else {
+            // dropdownContent.classList.remove('toggle')
+            // dropdownContent.classList.add('dropdown-content');
+
+            dropdownContent5.style.display = 'none'
+        }
+    })
+    var dropdownContent6 = document.querySelector('.dc6')
+    var mainContent = document.querySelector('.fb6')
+    mainContent.addEventListener('click', function () {
+        if (dropdownContent6.style.display === '' || dropdownContent.style.display === 'none') {
+
+            dropdownContent6.style.display = 'block'
+
+
+        }
+        else {
+            // dropdownContent.classList.remove('toggle')
+            // dropdownContent.classList.add('dropdown-content');
+
+            dropdownContent6.style.display = 'none'
+        }
+    })
